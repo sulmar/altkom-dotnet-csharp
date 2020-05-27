d4fa1155afe642438fa6982df4532f3ec0f6445c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-05-27 10:33:16 +0200

    EF

---
+++ src/Altkom.CSharp/Altkom.CSharp.EFServices/EFProductService.cs
@@ -0,0 +1,76 @@
+﻿using Altkom.CSharp.IServices;
+using Altkom.CSharp.Models;
+using Altkom.CSharp.Models.SearchCriterias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Altkom.CSharp.EFServices
+{
+    public class EFProductService : IProductService
+    {
+        private readonly MyContext context;
+
+
+        public EFProductService(MyContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(Product entity)
+        {
+            context.Items.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void AddRange(IEnumerable<Product> entities)
+        {
+            context.Items.AddRange(entities);
+            context.SaveChanges();
+        }
+
+        private IQueryable<Product> products => context.Items.OfType<Product>();
+
+        public int Count()
+        {
+            return products.Count();
+        }
+
+        public IEnumerable<Product> Get(string color)
+        {
+            return products
+                .Where(p => p.Color == color)
+                .OrderBy(p=>p.Name)
+                .ToList();
+        }
+
+        public IEnumerable<Product> Get(ProductSearchCriteria searchCriteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            return products.ToList();
+        }
+
+        public Product Get(int id)
+        {
+            return products.SingleOrDefault(product => product.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            context.Items.Remove(Get(id));
+            context.SaveChanges();
+        }
+
+        public void Update(Product entity)
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ src/Altkom.CSharp/Altkom.CSharp.EFServices/MyContext.cs
@@ -0,0 +1,23 @@
+﻿using Altkom.CSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom.CSharp.EFServices
+{
+    // PMC> Install-Package EntityFramework
+    public class MyContext : DbContext
+    {
+        public MyContext()
+            : base("MyConnection")
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
+}
